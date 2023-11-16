286978cf23b6a3dac64257c17cd239b8047bd1fe
ValeriyaZ <Valeriyazhmts@gmail.com>
2023-11-16 17:25:26 +0300

    занятие в унике1

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject obj;
+    public GameObject enemy;
     public GameObject _fx;
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        if (collision.collider.CompareTag(obj.tag))
+        if (collider.gameObject == enemy)
         {
             Instantiate(_fx, obj.transform.position, Quaternion.identity);
-            Destroy(gameObject); // Уничтожаем объект, к которому прикреплен скрипт
+            Destroy(obj); // Уничтожаем объект, к которому прикреплен скрипт
             Destroy(_fx);
         }
     }
