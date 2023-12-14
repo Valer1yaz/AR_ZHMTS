f1c50f6052758ce505e7458d860c68c218c736d0
user <Valeriyazhmts@gmail.com>
2023-12-14 18:23:31 +0300

    12345678

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -7,48 +7,14 @@ using UnityEngine;
 public class Book : MonoBehaviour
 {
     [SerializeField] PlayerMoney _playerMoney;
-    [SerializeField] GameObject _fx;
-    [SerializeField] GameObject _player;
-
-    [SerializeField] float _spawnDistance = 1;
-
-    [SerializeField] public int _price = 5;
     [SerializeField] public int _pointsForBird = 20;
-
-    public lb_Bird _bird;
-
 
     private void OnTriggerEnter(Collider collider)
     {
-
         if (collider.tag == "lb_bird")
         {
             _playerMoney.ProcessWin(_pointsForBird);
             _playerMoney.DeadBirdCount();
-        }
-    }
-    private void Start()
-    {
-        // ѕолучаем ссылку на игрока, если она не установлена
-        if (_player == null)
-        {
-            _player = GameObject.FindGameObjectWithTag("Player");
-        }
-    }
-
-    public void MoveObject()
-    {
-        Vector3 playerPos = _player.transform.position;
-        Vector3 playerDirection = _player.transform.forward;
-        Quaternion playerRotation = _player.transform.rotation;
-
-        Vector3 spawnPos = playerPos + playerDirection * _spawnDistance;
-
-        if (_playerMoney.CanBuy(_price))
-        {
-            transform.position = spawnPos;
-            _playerMoney.ProcessBuy(_price);
-            Instantiate(_fx, spawnPos, playerRotation);
         }
     }
 }
